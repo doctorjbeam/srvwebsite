2427cc035f1bb044a3824b5717ff32a58ada8abc
Michael Greenhill <doctorjbeam@gmail.com>
2015-03-06 09:22:57 +1100

    CSS updates and contact page updated
    Signed-off-by: Michael Greenhill <doctorjbeam@gmail.com>

---
+++ public_html/content/inc.contact.php
@@ -0,0 +1,199 @@
+<div class="main sidebar-right-margin">
+	<div class="feature-r761">
+		<h1>Email us</h1>
+		
+		<span>
+			<p><strong>General information</strong><br><a href="mailto:info@steamrail.com.au">info@steamrail.com.au</a></p>
+		</span>
+		
+		<span>
+			<p><strong>Charter enquiries</strong><br><a href="mailto:charter@steamrail.com.au">charter@steamrail.com.au</a></p>
+		</span>
+		
+		<span>
+			<p><strong>Volunteer information</strong><br><a href="mailto:volunteer@steamrail.com.au">volunteer@steamrail.com.au</a></p>
+		</span>
+		
+		<span>
+			<p><strong>Photo gallery submissions</strong><br><a href="mailto:steamrail@steamrail.com.au">steamrail@steamrail.com.au</a></p>
+		</span>
+		
+		<span>
+			<p><strong>Secretary</strong><br><a href="mailto:secretary@steamrail.com.au">secretary@steamrail.com.au</a></p>
+		</span>
+		
+		<span>
+			<p><strong>Booking officer</strong><br><a href="mailto:booking@steamrail.com.au">booking@steamrail.com.au</a></p>
+		</span>
+		
+		<span>
+			<p><strong>Membership officer</strong><br><a href="mailto:membership@steamrail.com.au">membership@steamrail.com.au</a></p>
+		</span>
+	</div>
+	
+	<div class="flexbox-container noborder nomaxheight">
+		<div class="flexbox flexbox-half">
+			<div>
+				<h3>Steamrail board of directors</h3>
+				
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Chairman</p>
+					<p class="flexbox flexbox-half">Mark Wolverson</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Secretary</p>
+					<p class="flexbox flexbox-half">Lionel Camilleri</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Treasurer</p>
+					<p class="flexbox flexbox-half">Graeme Harrison</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Board Member</p>
+					<p class="flexbox flexbox-half">Paul Constantinidis</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Board Member</p>
+					<p class="flexbox flexbox-half">Murray George</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Board Member</p>
+					<p class="flexbox flexbox-half">Michael McGlade</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Board Member</p>
+					<p class="flexbox flexbox-half">Jack Nicholson</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Board Member</p>
+					<p class="flexbox flexbox-half">David Pywell</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Board Member</p>
+					<p class="flexbox flexbox-half">Andrew Reynolds</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Board Member</p>
+					<p class="flexbox flexbox-half">Peter Sharpe</p>
+				</div>
+			</div>
+			
+			<div>
+				<h3>Social Media representatives</h3>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Facebook Admin</p>
+					<p class="flexbox flexbox-half">Les Coulton</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Facebook Admin</p>
+					<p class="flexbox flexbox-half">Lionel Camilleri</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Facebook Content</p>
+					<p class="flexbox flexbox-half">Henry Owen</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Facebook Content</p>
+					<p class="flexbox flexbox-half">Stephen Norgate</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Facebook Content</p>
+					<p class="flexbox flexbox-half">Tim Harris</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="flexbox flexbox-half">
+			<div>
+				<h3>Department managers</h3>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Operations manager</p>
+					<p class="flexbox flexbox-half">Les Coulton</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Marketing manager</p>
+					<p class="flexbox flexbox-half">Cathie Skelton</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Mechanical manager</p>
+					<p class="flexbox flexbox-half">Warren Hall</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Carriage manager</p>
+					<p class="flexbox flexbox-half">Brian Brand</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">DieselRail manager</p>
+					<p class="flexbox flexbox-half">Les Coulton</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Elecrail manager</p>
+					<p class="flexbox flexbox-half">Kevin Clark</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Rail Safety manager</p>
+					<p class="flexbox flexbox-half">Stuart Thyer</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Kiosk manager</p>
+					<p class="flexbox flexbox-half">Joan Norgate</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Newsletter editor</p>
+					<p class="flexbox flexbox-half">Cathie Skelton</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Website administrator</p>
+					<p class="flexbox flexbox-half">Les Coulton</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Booking officer</p>
+					<p class="flexbox flexbox-half">Stephen Norgate</p>
+				</div>
+					
+				<div class="flexbox-container tablerow">
+					<p class="flexbox flexbox-half">Content manager</p>
+					<p class="flexbox flexbox-half">Henry Owen</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="well well-right well-red">
+	<p>We welcome your enquiries, support and membership!</p>
+	<p>Please contact us by any of the methods below if you have a question / query / booking etc.</p>
+	<p><strong>Mailing address</strong><br>Steamrail Victoria Inc.<br>P.O. Box 125 Newport<br>Victoria 3015 Australia</p>
+	
+	<p>Registered in Victoria, Australia No. A0026264J<br>ABN: 46 460 156 080</p>
+	
+	<p>Mail completed booking forms to: <br>Steamrail Victoria Inc.<br>P.O. Box 125 Newport<br>Victoria 3015</p>
+	<p>or email:<br>booking@steamrail.com.au</p>
+
+	<p><strong>Phone</strong><br>03 9397 1953<br><strong>Fax</strong><br>03 9397 1041<br><strong>International Phone</strong><br>+61 3 9397 1953<br><strong>International Fax</strong><br>+61 3 9397 1041</p>
+</div>
--- public_html/content/inc.index.php
+++ public_html/content/inc.index.php
@@ -8,21 +8,22 @@
 <script src="vendor/mrclay/minify/min/?f=js/dot/doT.min.js,js/moment-with-locales.min.js,social-feed/js/jquery.socialfeed.js"></script>
 
 <div class="main sidebar-right-margin">
-	<div id="carousel">
+	<div id="carousel"></div>
 	
-	</div>
-	
-	<p>Step aboard and be transported back in time. </p>
+	<h2>Step aboard and be transported back in time</h2>
 	<p>From the moment you board a Steamrail train, you are riding living history in the beautiful timber panelled carriages, many still with fantastic Edwardian pressed tinwork on the ceilings. Up front is the real deal - a fully restored steam locomotive up to 107 years old, or heritage diesel dating from the 1950s, ready to take you away...</p>
 	
+	<!--
 	<div class="inset">
 		<h2>Steamrail Gallery</h2>
 		<p>See the latest images of recent tours, as well as our locomotives and rollingstock.</p>
 		<p>We are currently using our "Flickr" group for images and plans are in place to link this back to our website. Our old "Gallery" software is currently offline so this means the About Us pages for our Steam, Electric, Diesel and Carriage pages do not contain photos at this point in time. We hope to have this rectified as soon as possible. Some latest photos have also been uploaded to our Facebook page in the interim.</p>
 	</div>
+	//-->
 	
 	<div class="social">
 		<h2>Social</h2>
+		<img src="//static.railpage.com.au/i/loading-fb.gif">
 	</div>
 </div>
 
@@ -34,33 +35,35 @@
 <script>
 	$(document).ready(function() {
 		
-		/**
-		 * Draw the carousel
-		 */
-		
-		var $images = [
-			"https://farm4.staticflickr.com/3942/15444052659_f6b1953c8f_z.jpg",
-			"https://farm7.staticflickr.com/6213/6291035484_f3d88c42e0_z.jpg",
-			"https://farm4.staticflickr.com/3925/14639579631_a91e85e614_z.jpg",
-			"https://farm4.staticflickr.com/3880/14464434449_886c42401c_z.jpg",
-			"https://farm6.staticflickr.com/5528/14670921473_30dd786698_z.jpg",
-			"https://farm8.staticflickr.com/7352/11700713423_fbaa52eee8_z.jpg",
-			"https://farm4.staticflickr.com/3739/11699475415_7ec82dfdea_z.jpg"
-		];
-		
-		var $target = $("#carousel");
-		var $k = 99;
-		
-		window.setInterval(function() {
-			if (typeof $images[$k + 1] == "undefined") {
-				$k = 0; 
-			} else {
-				$k++;
-			}
+		if ($("#carousel").length) {
+			/**
+			 * Draw the carousel
+			 */
 			
-			$target.css("opacity", 0).css("background-image", "url(" + $images[$k] + ")").animate({ opacity : 1 }, { duration : 400 });
-		}, 4000);
-		
+			var $images = [
+				"https://farm4.staticflickr.com/3942/15444052659_f6b1953c8f_z.jpg",
+				"https://farm7.staticflickr.com/6213/6291035484_f3d88c42e0_z.jpg",
+				"https://farm4.staticflickr.com/3925/14639579631_a91e85e614_z.jpg",
+				"https://farm4.staticflickr.com/3880/14464434449_886c42401c_z.jpg",
+				"https://farm6.staticflickr.com/5528/14670921473_30dd786698_z.jpg",
+				"https://farm8.staticflickr.com/7352/11700713423_fbaa52eee8_z.jpg",
+				"https://farm4.staticflickr.com/3739/11699475415_7ec82dfdea_z.jpg"
+			];
+			
+			var $target = $("#carousel");
+			var $k = 99;
+			
+			window.setInterval(function() {
+				if (typeof $images[$k + 1] == "undefined") {
+					$k = 0; 
+				} else {
+					$k++;
+				}
+				
+				$target.css("opacity", 0).css("background-image", "url(" + $images[$k] + ")").animate({ opacity : 1 }, { duration : 400 });
+			}, 4000);
+		}
+			
 		/**
 		 * Social feed
 		 */
