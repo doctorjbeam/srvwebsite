a9d1b78c1563502ca5aeb0c25898d2f1d399c24b
SRVSecretary <secretary@steamrail.com.au>
2015-03-18 10:42:27 +1100

    Updated Various Pages
    Signed-off-by: SRVSecretary <secretary@steamrail.com.au>

--- public_html/content/inc.links.php
+++ public_html/content/inc.links.php
@@ -1,74 +1,424 @@
 <div class="main">
+
 	<h1>Victorian mainline heritage operators</h1>
+
 	
+
 	<div class="flexbox-container noborder small">
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href="http://www.707operations.com.au/">707 Operations</a></h2>
+
+			<h2><a href="http://www.707operations.com.au/" target="_blank">707 Operations</a></h2>
+
 			<p>707 Operations runs broad gauge mainline tours around Victoria. Their main attraction is Hudson R707.</p>
+
 		</div>
+
 		
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href="http://www.srhc.org.au/">Seymour Railway Heritage Centre</a></h2>
+
+			<h2><a href="http://www.srhc.org.au/" target="_blank">Seymour Railway Heritage Centre</a></h2>
+
 			<p>SRHC operates broad gauge mainline tours out of Seymour in central Victoria to all places in Victoria.</p>
+
 		</div>
+
 		
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href="http://www.dermpav.net.au/">DERMPAV</a></h2>
+
+			<h2><a href="http://www.dermpav.net.au/" target="_blank">DERMPAV</a></h2>
+
 			<p>DERMPAV is a non profit organisation operating out of part of the Newport Railway Workshops in Victoria, Australia. DERMPAV operate Diesel Electric Rail Motor Number 58 on various main line tours throughout the year.</p>
+
 		</div>
+
 	</div>
+
 	
+
 	<h1>Victorian tourist railways and museums</h1>
+
 	
+
 	<div class="flexbox-container noborder small">
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href="http://www.alexandratramway.org.au/">Alexandra Timber Tramway</a></h2>
+
+			<h2><a href="http://www.alexandratramway.org.au/" target="_blank">Alexandra Timber Tramway</a></h2>
+
 			<p>3ft narrow gauge railway located at Alexandra in central Victoria.</p>
+
 		</div>
+
 		
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href="http://www.atr.org.au/">Association of Tourist Railways</a></h2>
+
+			<h2><a href="http://www.atr.org.au/" target="_blank">Association of Tourist Railways</a></h2>
+
 			<p>The peak body for tourist railway industry located in Victoria.</p>
+
 		</div>
+
 		
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href="http://www.arhsvic.org.au/">Australian Railway Historical Society (VIC)</a></h2>
+
+			<h2><a href="http://www.arhsvic.org.au/" target="_blank">Australian Railway Historical Society (VIC)</a></h2>
+
 			<p>The Victorian division of the ARHS and North Williamstown railway museum.</p>
+
 		</div>
+
 		
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href=""></a></h2>
-			<p></p>
+
+			<h2><a href="http://www.btm.org.au/" target="_blank">The Ballarat Tramway Museum</a></h2>
+
+			<p>A Tramway Museum located in the goldfields district of Ballarat.</p>
+
 		</div>
+
 		
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href=""></a></h2>
-			<p></p>
+
+			<h2><a href="http://bellarinerailway.com.au/" target="_blank">The Bellarine Railway</a></h2>
+
+			<p>A 3ft 6 narrow gauge railway that runs between Queenscliff and Drysdale.</p>
+
 		</div>
+
 		
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href=""></a></h2>
-			<p></p>
+
+			<h2><a href="http://www.bendigotramways.com/" target="_blank">Bendigo Tramways</a></h2>
+
+			<p>Bendigo tramway website, home of the 'Talking Tram'</p>
+
 		</div>
+
 		
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href=""></a></h2>
-			<p></p>
+
+			<h2><a href="http://www.coalcreekvillage.com.au/" target="_blank">Coal Creek Bush Tramway</a></h2>
+
+			<p>A small bush tramway located in Coal Creek which is at Korumburra</p>
+
 		</div>
+
 		
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href=""></a></h2>
-			<p></p>
+
+			<h2><a href="http://www.dscr.com.au/" target="_blank">Daylesford Spa Country Railway</a></h2>
+
+			<p>An accredited museum re-creating the days of Victorian Railways branch line railmotor travel</p>
+
 		</div>
+
 		
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href=""></a></h2>
-			<p></p>
+
+			<h2><a href="http://www.mtpa.com.au/" target="_blank">Melbourne Tramcar Preservation Association</a></h2>
+
+			<p>A group preserving a number of Melbourne's old trams</p>
+
 		</div>
+
 		
+
 		<div class="flexbox flexbox-half margin--bottom">
-			<h2><a href=""></a></h2>
-			<p></p>
+
+			<h2><a href="http://www.morningtonrailway.org.au/" target="_blank">Mornington Railway</a></h2>
+
+			<p>A broad gauge tourist railway that runs between Moorooduc and Mornington</p>
+
 		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://portlandcabletrams.com.au//" target="_blank">Portland Cable Trams</a></h2>
+
+			<p>The Cable Tram railway located in Victoria's south-west in Portland</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.puffingbilly.com.au/" target="_blank">Puffing Billy</a></h2>
+
+			<p>The famous Dandenong's Puffing Billy that runs between Belgrave and Gembrook</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.visitmelbourne.com/Regions/The-Murray/Things-to-do/History-and-heritage/Red-Cliffs-Historical-Steam-Railway.aspx" target="_blank">Red Cliffs Historical Steam Railway</a></h2>
+
+			<p>A small narrow gauge railway at Red Cliffs near Mildura</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.sgr.org.au/" target="_blank">The South Gippsland Railway</a></h2>
+
+			<p>A broad gauge tourist railway that runs between Leongatha at Nyora in the Strezlecki ranges. The SGR's depot is located at Korumburra</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.tramway.org.au/" target="_blank">Tramway Museum Society Victoria</a></h2>
+
+			<p>The tramway museum society of Victoria</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.vgr.com.au/" target="_blank">Victorian Goldfields Railway</a></h2>
+
+			<p>A broad gauge tourist railway that runs between Maldon and Castlemaine</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.vicsteam.com/" target="_blank">Victorian Steam Locomotive Company</a></h2>
+
+			<p>To build and operate a Victorian Railways, Vauclain compound cylinder, V class, 2-8-0.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.walhallarail.com/" target="_blank">Walhalla Goldfields Railway</a></h2>
+
+			<p>A 2ft 6 narrow gauge railway deep in Stringers Creek Gorge between Walhalla and Thomson in Victoria's east.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.yvr.com.au/" target="_blank">Yarra Valley Railway</a></h2>
+
+			<p>A broad gauge railway that runs out of Healsville</p>
+
+		</div>
+
 	</div>
+    
+    
+    
+    <h1>Victorian miniature railways</h1>
+
+	
+
+	<div class="flexbox-container noborder small">
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.dvr.com.au/" target="_blank">Diamond Valley Railway</a></h2>
+
+			<p>A 7inch live steam railway located at Eltham in Melbourne's north-east.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://gmes.org.au/" target="_blank">Gippsland Model Engineering Society</a></h2>
+
+			<p>A 7inch live steam railway located at Traralgon in Victoria's south east.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://altonaminiaturerailway.com.au/" target="_blank">Altona Miniature Railway</a></h2>
+
+			<p>A live steam railway at Altona in Melbourne, has both 7inch and 5inch gauges.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.bhmsrs.org.au/" target="_blank">Box Hill  Miniature Steam Railway</a></h2>
+
+			<p>A live steam railway at Box Hill with both 7inch and 5inch.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.gatewaybbs.com.au/mintrain/index.htm" target="_blank">Cobden Miniature Railway</a></h2>
+
+			<p>A live steam railway located at Cobden in Victoria's south-west.</p>
+
+		</div>
+
+	</div>
+    
+    
+    
+    <h1>Victorian railway operators</h1>
+
+	
+
+	<div class="flexbox-container noborder small">
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.vline.com.au/" target="_blank">V/Line</a></h2>
+
+			<p>Victoria's passenger train operator.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.metrotrains.com.au/" target="_blank">Metro Trains</a></h2>
+
+			<p>Melbourne's suburban network operator.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://asciano.com.au/pacific-national" target="_blank">Pacific National</a></h2>
+
+			<p>Australia's leading freight carrier.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.aurizon.com.au/" target="_blank">Aurizon (previously QR National)</a></h2>
+
+			<p>Queensland Based, moving freight all over Australia, previously owned by the Queensland government.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.artc.com.au/" target="_blank">Australian Rail Track Corporation</a></h2>
+
+			<p>A one stop shop for all operators seeking access to the National interstate rail network.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.cfcla.com.au/" target="_blank">Chicago Freight Car Leasing Australia</a></h2>
+
+			<p>Leases rollingstock to operators all over Australia.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.dtpli.vic.gov.au/transport" target="_blank">Department of Transport (Planning and Local Infrastructure)</a></h2>
+
+			<p>The Department of Transport in Victoria. (DTPLI)</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="https://www.victrack.com.au/" target="_blank">VicTrack</a></h2>
+
+			<p>Created in 1997, VicTrack is a State-owned business enterprise with an independent Board and plays a pivotal role supporting the Victorian Government to provide improved public transport outcomes without relying on government funding.</p>
+
+		</div>
+
+		
+
+	  <div class="flexbox flexbox-half margin--bottom">
+
+		<h2><a href="http://www.greatsouthernrail.com.au/" target="_blank">Great Southern Rail</a></h2>
+
+		  <p>Operators of 'The Ghan', 'The Overland', and the 'Indian Pacific'.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://www.theworkshops.qm.qld.gov.au/" target="_blank">The Workshops Rail Museum</a></h2>
+
+			<p>Government owned workshops in Queensland for restoration of heritage trains.</p>
+
+		</div>
+
+		
+
+		<div class="flexbox flexbox-half margin--bottom">
+
+			<h2><a href="http://asciano.com.au/pacific-national" target="_blank">Pacific National</a></h2>
+
+			<p>Australia's leading freight carrier.</p>
+
+		</div>
+
+		
+
+	  <div class="flexbox flexbox-half margin--bottom">
+
+		<h2><a href="http://espee.com.au/" target="_blank">ESPEE Railroad Services (ACT)</a></h2>
+
+		  <p>ESPEE Railroad Services is an accredited operator in New South Wales, South Australia and Victoria. ESPEE is a wholly-owned business of the <a href="http://canberrarailwaymuseum.org/arhs-act" target="_blank">Australian Railway Historical Society (ACT Division) Inc</a>. that has been trading since 1974.</p>
+
+		</div>
+
+	</div>
+
+
 </div>
